3bef3660b46ce26f5252210b41d9b6e22b0d7466
ankit <ankit2337@gmail.com>
2019-07-28 17:48:14 +0530

    added into git master file

--- src/Alpha/Demo.java
+++ src/Alpha/Demo.java
@@ -4,7 +4,9 @@ public class Demo {
 	static Logger log=LogManager.getLogger(Demo.class.getName());
 	public static void main(String[] args) {
 	log.debug("This is debuging message");	
-		
+    
+	log.debug("This is debuging message added in cone appliation git");	
+
 	log.error("this is error message");	
 	}
 
